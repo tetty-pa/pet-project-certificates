92c91fe3428b3f905639d5d47876e951d33a059a
tetty-pa <tetty.pavlyshyn@gmail.com>
2023-08-29 17:26:07 +0300

    Removed * from imports in RestResponseEntityExceptionHandler

--- web/src/main/java/com/epam/esm/web/exception/RestResponseEntityExceptionHandler.kt
+++ web/src/main/java/com/epam/esm/web/exception/RestResponseEntityExceptionHandler.kt
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import java.util.*
+import java.util.Locale
 
 @RestControllerAdvice
 class RestResponseEntityExceptionHandler(private val messageSource: MessageConfig) {
