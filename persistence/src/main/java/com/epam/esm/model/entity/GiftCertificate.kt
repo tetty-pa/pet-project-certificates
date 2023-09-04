7b427e08e49bcbd7758b1dc1ee2298a370f345b1
tetty-pa <tetty.pavlyshyn@gmail.com>
2023-09-04 12:34:58 +0300

    Changed mysql entity to mongo pojos

--- persistence/src/main/java/com/epam/esm/model/entity/GiftCertificate.kt
+++ persistence/src/main/java/com/epam/esm/model/entity/GiftCertificate.kt
@@ -3,61 +3,41 @@ package com.epam.esm.model.entity
 import com.epam.esm.model.entity.audit.EntityAuditListener
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonProperty
-import jakarta.persistence.CascadeType
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
 import jakarta.persistence.EntityListeners
-import jakarta.persistence.FetchType
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.JoinTable
-import jakarta.persistence.ManyToMany
-import jakarta.persistence.Table
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.Size
+import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.hateoas.RepresentationModel
 import java.math.BigDecimal
-import java.time.ZonedDateTime
+import java.time.LocalDateTime
 
-@Entity
-@Table(name = "gift_certificates")
+
+@Document("gift_certificates")
 @EntityListeners(EntityAuditListener::class)
-class GiftCertificate(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = 0,
-
-    @Column(length = 80)
+data class GiftCertificate(
     @field:Size(min = 1, max = 80, message = "gift-certificate.invalidName")
-    var name: String = "",
+    var name: String,
 
-    @Column(length = 250)
     @field: Size(min = 1, max = 250, message = "gift-certificate.invalidDescription")
-    var description: String = "",
+    var description: String,
 
     @field:Min(value = 1, message = "gift-certificate.invalidPrice")
-    var price: BigDecimal = BigDecimal.ZERO,
+    var price: BigDecimal,
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
-    @Column(name = "create_date", updatable = false)
-    var createDate: ZonedDateTime? = null,
+    var createDate: LocalDateTime,
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
-    @Column(name = "last_updated_date")
-    var lastUpdatedDate: ZonedDateTime? = null,
+    var lastUpdatedDate: LocalDateTime,
 
     @field:Min(value = 1, message = "gift-certificate.invalidDuration")
-    var duration: Int = 0,
+    var duration: Int,
 
-    @JoinTable(
-        name = "gift_certificate_has_tag",
-        joinColumns = [JoinColumn(name = "gift_certificate_id", referencedColumnName = "ID")],
-        inverseJoinColumns = [JoinColumn(name = "tag_id", referencedColumnName = "ID")]
-    )
-    @ManyToMany(cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
-    var tagList: MutableList<Tag> = mutableListOf()
-) : RepresentationModel<GiftCertificate>()
+    val tagList: MutableList<Tag>
+) : RepresentationModel<GiftCertificate>() {
+    @Id
+    lateinit var id: String
+}
