9570fa387161dd7f1e91faceec2601510960b0de
tetty-pa <tetty.pavlyshyn@gmail.com>
2023-09-15 12:20:19 +0300

    Added missing file

---
+++ nats/build.gradle.kts
@@ -0,0 +1,52 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    idea
+    java
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("com.google.protobuf") version "0.9.4"
+
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.10"
+    kotlin("plugin.allopen") version "1.9.10"
+}
+
+group = "com.epam.esm"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation("io.nats:jnats:2.16.14")
+    implementation("com.google.protobuf:protobuf-java:3.24.2")
+    implementation("com.google.protobuf:protobuf-java-util:3.20.1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    languageVersion = "1.9"
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks {
+    bootJar {
+        enabled = false
+    }
+}
