7452559b554e00314c1e985f27f1334796a91359
tetty-pa <83034143+tetty-pa@users.noreply.github.com>
2023-10-07 17:08:09 +0300

    Update nats/src/main/kotlin/com/epam/esm/KafkaTopic.kt
    Co-authored-by: Sergii Gnatiuk <116563306+ajax-gnatiuk-s@users.noreply.github.com>

--- nats/src/main/kotlin/com/epam/esm/KafkaTopic.kt
+++ nats/src/main/kotlin/com/epam/esm/KafkaTopic.kt
@@ -1,5 +1,5 @@
 package com.epam.esm
 
 object KafkaTopic {
-    const val ADD_GIFT_CERTIFICATE_TOPIC = "nats_add_gift_certificate"
+    const val ADD_GIFT_CERTIFICATE_TOPIC = "kafka_add_gift_certificate"
 }
